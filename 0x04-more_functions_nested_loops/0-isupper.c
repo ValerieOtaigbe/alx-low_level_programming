7157d3269c0aa2689e9571dc175f51b517cf6979
Valerie <otaigbevalerie@gmail.com>
2022-12-16 01:53:34 -0800

    test if upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main- check the code
+ *
+ * Return: always 0.
+ */
+int _isupper(void)
+{
+	int c;
+
+	if (c >= 65 && c <= 90)
+
+		return (1);
+	else
+		return (0);
+}
