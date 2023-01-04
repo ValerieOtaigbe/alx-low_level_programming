6a1c2bff469ab54cc27d6531dd34fb980a1db329
Valerie <otaigbevalerie@gmail.com>
2023-01-04 15:18:46 -0800

    hfjj

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse.
+ * _puts_recursion - prints a string, followed by a new line.
  * @s: string
  * Return: no return.
  */
 
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s + 1);
 		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
